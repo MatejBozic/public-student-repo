3cf0dcf2953789229b4a56a18134e4067334bef7
Matej Bozic <matej@Matejs-MacBook-Pro.local>
2024-04-17 21:24:25 -0700

    Changed a function and functionality.
    Changed the 'handleInput' function from a declaration to an expression.
    
    Changed the 'checkIfInputIsPalindrome' function so it works for floating points too.

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,100 +1,42 @@
 const elem = document.querySelector('input');
 
+const handleInput = function handleInput() {
+  if (inputIsValid(elem.value)) {
+    checkIfInputIsPalindrome(elem.value);
+  }
+};
+
 elem.addEventListener('input', handleInput);
 
-function handleInput() {
-  destroyResponse();
-  if (inputIsValid(elem.value)) {
-    if (inputIsPalindrome(elem.value)) {
-      createResponse('Yes. This is a palindrome!');
-    } else {
-      createResponse('No. Try again.');
-    }
-  }
-}
-
 const inputIsValid = function inputIsValid(input) {
-  if (inputIsEmpty(input)) {
-    return false;
-  } else if (inputIsNegative(input)) {
-    return false;
-  } else if (inputIsFloat(input)) {
-    return false;
-  } else {
-    return true;
-  }
-};
-
-const inputIsEmpty = function inputIsEmpty(input) {
   if (input === '') {
-    return true;
-  } else {
+    modifyResponse('');
     return false;
+  } else if (isNaN(input)) {
+    modifyResponse('This is not a number.', 'text-danger');
+    return false;
+  } else if (input.includes('-')) {
+    modifyResponse('This number is not positive.', 'text-danger');
+    return false;
+  } else {
+    return true;
   }
 };
 
-const inputIsNegative = function inputIsNegative(input) {
-  if (input.includes('-')) {
-    createResponse('No negative numbers.');
-    return true;
-  } else {
-    return false;
-  }
-};
-
-const inputIsFloat = function inputIsFloat(input) {
-  if (input.includes('.')) {
-    createResponse('No floating point numbers.');
-    return true;
-  } else {
-    return false;
-  }
-};
-
-const inputIsPalindrome = function inputIsPalindrome(value) {
-  let number = Number(value);
-  let digitReversedNumber = parseInt(
-    number.toString().split('').reverse().join('')
-  );
-  if (number === digitReversedNumber) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-const destroyResponse = function destroyResponse() {
+const modifyResponse = function modifyResponse(text, textColor) {
   const response = document.getElementById('response');
-  if (response !== null) {
-    response.remove();
-  }
+  response.textContent = text;
+  response.setAttribute('class', textColor);
 };
 
-const createResponse = function createResponse(outputText) {
-  const newParagraph = document.createElement('p');
-  const newText = document.createTextNode(outputText);
-  const textColor = determineCorrectTextColorFor(outputText);
+const checkIfInputIsPalindrome = function checkIfInputIsPalindrome(input) {
+  let inputReversed = input.split('').reverse().join('');
 
-  newParagraph.appendChild(newText);
-  newParagraph.setAttribute('id', 'response');
-  newParagraph.setAttribute('class', textColor);
-
-  const appendResponseHereDiv = document.getElementById('appendResponseHere');
-  appendResponseHereDiv.append(newParagraph);
-};
-
-const determineCorrectTextColorFor = function determineCorrectTextColorFor(
-  outputText
-) {
-  if (outputText.includes('Yes')) {
-    return 'text-success';
-  } else if (
-    outputText.includes('No') ||
-    outputText.includes('negative') ||
-    outputText.includes('floating point')
-  ) {
-    return 'text-danger';
+  if (input === inputReversed) {
+    modifyResponse('Yes. This is a palindrome!', 'text-success');
+    return true;
   } else {
-    console.log('Unknown input for the function determineCorrectTextColorFor.');
+    modifyResponse('No. Try again.', 'text-danger');
+    return false;
   }
 };
