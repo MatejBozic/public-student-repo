b4339ff2f0ec2cc2090e071db20e9ae8c09042a5
Matej Bozic <matej@Matejs-MacBook-Pro.local>
2024-04-22 19:12:20 -0700

    Attempted: Exercise 03 - Count

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,3 +1,36 @@
-// Add your code here
+const originalTextUnhighlightedState =
+  document.getElementById('givenText').innerHTML;
 
-input.addEventListener('keydown', handleKeyDown);
+const userInputDiv = document.getElementById('userInput');
+
+const handleKeyUp = function handleKeyUp() {
+  resetGivenTextToOriginalUnhighlightedState();
+  hightlightGivenTextBasedOnUserInput();
+};
+userInputDiv.addEventListener('keyup', handleKeyUp);
+
+const resetGivenTextToOriginalUnhighlightedState =
+  function resetGivenTextToOriginalUnhighlightedState() {
+    const givenText = document.getElementById('givenText');
+    givenText.innerHTML = originalTextUnhighlightedState;
+  };
+
+const hightlightGivenTextBasedOnUserInput =
+  function hightlightGivenTextBasedOnUserInput() {
+    const userInput = userInputDiv.value;
+    if (userInput !== '') {
+      // Get given text
+      const givenTextDiv = document.getElementById('givenText');
+      const givenText = givenTextDiv.innerHTML;
+
+      // Create regular expression
+      const regex = new RegExp(`[^\\w]${userInput}[^\\w]`, 'gi');
+
+      // Replace given text with highlighted user input value
+      let highlightedText = givenText.replaceAll(
+        regex,
+        `<mark style="background-color: yellow;" class="p-0">${userInput}</mark>`
+      );
+      givenTextDiv.innerHTML = highlightedText;
+    }
+  };
