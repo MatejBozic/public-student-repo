12ffc37a0813669e8cc9fa3cc99e5c526b855f91
Matej Bozic <matej@Matejs-MacBook-Pro.local>
2024-05-26 05:08:45 -0700

    Added image of South America to the Home page.

--- hw3/src/routes/Main.jsx
+++ hw3/src/routes/Main.jsx
@@ -1,9 +1,19 @@
 import './Home.css';
+import Image from 'react-bootstrap/Image';
+import ImageOfSouthAmerica from '../images/SouthAmerica.jpg';
 
 export default function Home() {
   return (
-    <div id="Welcome" className="d-flex justify-content-center m-4 py-4">
-      <h1>Welcome</h1>
-    </div>
+    <>
+      <div id="Welcome" className="d-flex justify-content-center m-4 py-4">
+        <h1>Welcome</h1>
+      </div>
+      <div
+        className="d-flex justify-content-center"
+        style={{ height: '25rem' }}
+      >
+        <Image src={ImageOfSouthAmerica} />
+      </div>
+    </>
   );
 }
