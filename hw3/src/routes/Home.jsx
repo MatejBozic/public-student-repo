81f89ce9d0f5f32057754b6453bd41e326781f16
Matej Bozic <matej@Matejs-MacBook-Pro.local>
2024-05-26 05:27:22 -0700

    Added text describing app to the home page.

--- hw3/src/routes/Home.jsx
+++ hw3/src/routes/Home.jsx
@@ -8,11 +8,18 @@ export default function Home() {
       <div id="Welcome" className="d-flex justify-content-center m-4 py-4">
         <h1>Welcome</h1>
       </div>
+      <div className="d-flex justify-content-center text-center m-3">
+        <p>
+          In this application, you will find the official languages, population,
+          and GDP of various South American countries.
+        </p>
+      </div>
+
       <div
         className="d-flex justify-content-center"
         style={{ height: '25rem' }}
       >
-        <Image src={ImageOfSouthAmerica} />
+        <Image src={ImageOfSouthAmerica} alt={'Map of South America'} />
       </div>
     </>
   );
